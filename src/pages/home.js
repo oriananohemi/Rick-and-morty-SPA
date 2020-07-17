09bd0f475e5ee36e20cc5249fca8b94f86083c74
Oriana <oriana.nohemi22@gmail.com>
2020-07-16 21:59:54 -0500

    pintar personajes en el home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,21 @@
-const Home = () => {
+import getData from '../utils/getData'
+
+const Home = async () => {
+  const characters = await getData();
+
   const view = `
   <div class="characters">
+  ${characters.results.map(
+    character => `
     <article class="character-item>
-      <a href="#/1/">
-        <img src="image" alt="name>
-        <h2>Name</h2>
+      <a href="#/${character.id}/">
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
       </a>
     </article>
-  </div>
+    `
+    )}
+    </div>
   `
   return view
 }
