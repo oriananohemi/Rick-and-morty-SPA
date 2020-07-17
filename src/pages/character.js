c34a42744cb5617f6bb37da01b20e0ea1c27179e
Oriana <oriana.nohemi22@gmail.com>
2020-07-16 20:19:45 -0500

    template de header, caracteristicas

---
+++ src/pages/character.js
@@ -0,0 +1,33 @@
+const Character = () => {
+  const viwe = `
+  <div class="characters-inner">
+  <article class="caracters-card">
+  <img src="image" alt="name">
+  <h2>
+  Name
+  </h2>
+  </article>
+  <article class="Characters-card>
+  </article>
+    <h3>
+      Episodes:
+    </h3>
+    <h3>
+      Status:
+    </h3>
+    <h3>
+      Gender:
+    </h3>
+    <h3>
+      Origin:
+    </h3>
+    <h3>
+      Last Location:
+    </h3>
+  </article>
+  </.div>
+  `
+  return view
+}
+
+export default Character
